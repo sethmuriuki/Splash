ad8bc954a4f78824a1b80feb60ebca95fb85adc5
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-19 15:51:42 +0300

    update settings

--- splash/splash/settings.py
+++ splash/splash/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'splash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'splash',
+        'USER': 'seth',
+        'PASSWORD': 'seth',
     }
 }
 

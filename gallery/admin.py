fa663af0e18bf45f83840197f5dd9fde7e99030b
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-19 16:06:46 +0300

    update admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class ArticlesAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+    
+admin.site.register(Editor)
+admin.site.register(Articles)
+admin.site.register(tags)
---
+++ gallery/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url, include
+from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static

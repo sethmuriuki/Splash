c41f8047e6f97b0fd04d535b58c89ced5cb45878
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-21 22:04:08 +0300

    update models

--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Gallery(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    gallery_image = models.ImageField(upload_to = 'gallery/', null = True, blank = True)
-
-
 class categories(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to = 'galleries/', null = True)
+    name = models.CharField(max_length=30)
+    descripton = models.TextField()
+    category = models.ManyToManyField(categories)
+    time_uloaded = models.DateTimeField(auto_now_add=True, null=True)
+    

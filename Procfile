5d33ee4c7ff23d9faa70f53447e940482ab3c57f
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-24 11:37:16 +0300

    deploy to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn manage:app
+web: gunicorn SPLASH:wsgi
--- splash/settings.py
+++ splash/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'splash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

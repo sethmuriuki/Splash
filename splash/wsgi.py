49289480f58e16e7106acb2bf2efe2e7d1a965a1
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-24 12:42:23 +0300

    Deploy to heroku.

--- splash/wsgi.py
+++ splash/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "splash.settings")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)

73a7b10f014679897ef58ddb95a45f07416cb363
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-25 12:44:31 +0300

    add functions to add and delete category

--- gallery/models.py
+++ gallery/models.py
@@ -8,6 +8,12 @@ class categories(models.Model):
     def __str__(self):
         return self.name
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
